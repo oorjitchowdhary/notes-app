a83ec3638cf18b1b121f9df460ea0640e6f8fd49
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-05-20 22:47:48 +0530

    major commit - firebase

--- app/src/main/java/com/example/notes_app/MainActivity.java
+++ app/src/main/java/com/example/notes_app/MainActivity.java
@@ -1,38 +1,55 @@
 package com.example.notes_app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
-    EditText userName;
-    EditText passWord;
+private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        userName = (EditText)findViewById(R.id.username);
-        passWord = (EditText)findViewById(R.id.password);
-        login = (Button)findViewById(R.id.login);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validate(userName.getText().toString(), passWord.getText().toString());
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    private void validate (String userName, String userPassword) {
-        if (userName.equals("oorjit") && userPassword.equals("gpw2018")) {
-            Intent intent = new Intent(MainActivity.this, CreateNoteActivity.class);
-            startActivity(intent);
+
+        public void signUp(View view) {
+            EditText loginet=findViewById(R.id.email);
+            EditText passwordet=findViewById(R.id.password);
+            String email=loginet.getText().toString();
+            String password=passwordet.getText().toString();
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getApplicationContext(),"Sign up successful.",Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(MainActivity.this,CreateNoteActivity.class));
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(getApplicationContext(),"Sign up failed.",Toast.LENGTH_LONG).show();
+                            }
+
+                            // ...
+                        }
+                    });
         }
+
     }
-}
+
+
--- app/src/main/java/com/example/notes_app/ViewNotesActivity.java
+++ app/src/main/java/com/example/notes_app/ViewNotesActivity.java
@@ -1,13 +1,26 @@
 package com.example.notes_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewNotesActivity extends AppCompatActivity {
+    Button add_note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_notes);
+
+        add_note = (Button)findViewById(R.id.create_note);
+        add_note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewNotesActivity.this,CreateNoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
